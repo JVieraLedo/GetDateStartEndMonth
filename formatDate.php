fc78d64cd6469ed85589ffacb4ac941191e9a064
JoseViera <josvile@gmail.com>
2016-09-09 10:31:39 +0200

    add new value to method
    Now u can choose if its start of month or last day

--- formatDate.php
+++ formatDate.php
@@ -1,16 +1,16 @@
-public static function formatearFechaConMes($fechaIN, $month)
+public static function formatearFechaConMes($fechaIN, $month, $place)
     {
         $aDate = explode('/', $fechaIN);
-        switch ($month[0]) {
-            case '-':
-                $fechaOUT = strtotime('last day of ' . $month . ' month', strtotime(date($aDate[2] . '-' . $aDate[1] . '-' . $aDate[0])));
-                break;
-            case '+':
+        switch ($place) {
+            case 'start':
                 $fechaOUT = strtotime('first day of ' . $month . ' month', strtotime(date($aDate[2] . '-' . $aDate[1] . '-' . $aDate[0])));
+                break;
+            case 'end':
+                $fechaOUT = strtotime('last day of ' . $month . ' month', strtotime(date($aDate[2] . '-' . $aDate[1] . '-' . $aDate[0])));
                 break;
             default:
                 $fechaOUT = strtotime('first day of +' . $month . ' month', strtotime(date($aDate[2] . '-' . $aDate[1] . '-' . $aDate[0])));
                 break;
         }
-        return date('Y-m-d', $fechaOUT);
+        return date('d/m/Y', $fechaOUT);
     }
